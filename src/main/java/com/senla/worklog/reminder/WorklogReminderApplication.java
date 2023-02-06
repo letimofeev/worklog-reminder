fa471fb17c52feb64c8b22e453b1d61bf95a5bc6
leonid <kotovpetia@gmail.com>
2023-02-06 14:48:00 +0400

    Initialize spring boot application

---
+++ src/main/java/com/senla/worklog/reminder/WorklogReminderApplication.java
@@ -0,0 +1,11 @@
+package com.senla.worklog.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorklogReminderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WorklogReminderApplication.class, args);
+	}
+}
