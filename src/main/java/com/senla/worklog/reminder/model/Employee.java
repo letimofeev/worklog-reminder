87ba939b52a7506ce87a184c45fd0bd6b55c03c6
leonid <kotovpetia@gmail.com>
2023-02-06 20:56:12 +0400

    Add Employee entity and liquibase migrations

---
+++ src/main/java/com/senla/worklog/reminder/model/Employee.java
@@ -0,0 +1,43 @@
+package com.senla.worklog.reminder.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees", uniqueConstraints = {
+        @UniqueConstraint(name = "uc_employee_jirakey", columnNames = {"jiraKey"})
+})
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String jiraKey;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", jiraKey='" + jiraKey + '\'' +
+                '}';
+    }
+}
