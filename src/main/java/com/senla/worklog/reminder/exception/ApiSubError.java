77e819c01e5c68aadff8ada7cb1d0c83c0972a87
leonid <kotovpetia@gmail.com>
2023-02-08 01:15:33 +0400

    Add handling MethodArgumentTypeMismatchException

--- src/main/java/com/senla/worklog/reminder/exception/ApiSubError.java
+++ src/main/java/com/senla/worklog/reminder/exception/ApiSubError.java
@@ -1,4 +1,21 @@
 package com.senla.worklog.reminder.exception;
 
-public abstract class ApiSubError {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiSubError {
+    private String details;
+
+    @Override
+    public String toString() {
+        return "ApiSubError{" +
+                "details='" + details + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/senla/worklog/reminder/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/senla/worklog/reminder/exception/handler/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.senla.worklog.reminder.exception.handler;
 
 import com.senla.worklog.reminder.exception.ApiError;
+import com.senla.worklog.reminder.exception.ApiSubError;
 import com.senla.worklog.reminder.exception.EmployeeNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
@@ -14,5 +19,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError> handleException(EmployeeNotFoundException e) {
         ApiError apiError = new ApiError(e.getMessage(), NOT_FOUND);
         return new ResponseEntity<>(apiError, NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiError> handleException(MethodArgumentTypeMismatchException e) {
+        String message = "Failed to parse request parameter with name '" + e.getName() +
+                "' and value '" + e.getValue() + "'";
+        ApiSubError apiSubError = new ApiSubError(e.getMostSpecificCause().getMessage());
+        ApiError apiError = new ApiError(message, BAD_REQUEST, List.of(apiSubError));
+        return new ResponseEntity<>(apiError, BAD_REQUEST);
     }
 }
