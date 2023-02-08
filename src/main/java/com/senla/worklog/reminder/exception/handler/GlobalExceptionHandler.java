ad25c80e748d50897a5d4d67fd32e2d713e6c6c6
leonid <kotovpetia@gmail.com>
2023-02-08 13:37:18 +0400

    Add Exception handling + exceptions logging

--- src/main/java/com/senla/worklog/reminder/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/senla/worklog/reminder/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.senla.worklog.reminder.exception.handler;
 import com.senla.worklog.reminder.exception.ApiError;
 import com.senla.worklog.reminder.exception.ApiSubError;
 import com.senla.worklog.reminder.exception.EmployeeNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,19 +12,21 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<ApiError> handleException(EmployeeNotFoundException e) {
+        log.warn("Resolved EmployeeNotFoundException: {}", e.getMessage());
         ApiError apiError = new ApiError(e.getMessage(), NOT_FOUND);
         return new ResponseEntity<>(apiError, NOT_FOUND);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ApiError> handleException(MethodArgumentTypeMismatchException e) {
+        log.warn("Resolved MethodArgumentTypeMismatchException: {}", e.getMessage());
         String message = "Failed to parse request parameter with name '" + e.getName() +
                 "' and value '" + e.getValue() + "'";
         ApiSubError apiSubError = new ApiSubError(e.getMostSpecificCause().getMessage());
@@ -33,8 +36,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<ApiError> handleException(MissingServletRequestParameterException e) {
+        log.warn("Resolved MissingServletRequestParameterException: {}", e.getMessage());
         String message = "Missing value for parameter '" + e.getParameterName() + "'";
         ApiError apiError = new ApiError(message, BAD_REQUEST);
         return new ResponseEntity<>(apiError, BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception e) {
+        log.warn("Resolved {}: {}", e.getClass().getSimpleName(), e.getMessage());
+        e.printStackTrace();
+        String message = "Internal server error";
+        ApiError apiError = new ApiError(message, INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiError, INTERNAL_SERVER_ERROR);
     }
 }
