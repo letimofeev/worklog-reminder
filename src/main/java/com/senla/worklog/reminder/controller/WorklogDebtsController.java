999d00493cc42091e0626f9039ae1a466289b07c
leonid <kotovpetia@gmail.com>
2023-02-08 14:53:58 +0400

    Add WorklogDebtsController getAllDebtsFrom dateFrom validation

--- src/main/java/com/senla/worklog/reminder/controller/WorklogDebtsController.java
+++ src/main/java/com/senla/worklog/reminder/controller/WorklogDebtsController.java
@@ -5,15 +5,18 @@ import com.senla.worklog.reminder.dto.mapper.WorklogDebtsDtoMapper;
 import com.senla.worklog.reminder.model.WorklogDebts;
 import com.senla.worklog.reminder.service.WorklogDebtsService;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
 
+@Validated
 @RestController
 @RequestMapping("/worklog-debts")
 public class WorklogDebtsController {
@@ -32,7 +35,10 @@ public class WorklogDebtsController {
     }
 
     @GetMapping(params = {"dateFrom"})
-    public WorklogDebtsDto getAllDebtsFrom(@RequestParam @DateTimeFormat(iso = DATE) LocalDate dateFrom) {
+    public WorklogDebtsDto getAllDebtsFrom(@Past(message = "'dateFrom' without specifying 'dateTo' must be past date")
+                                           @RequestParam
+                                           @DateTimeFormat(iso = DATE)
+                                           LocalDate dateFrom) {
         LocalDate dateTo = LocalDate.now();
         WorklogDebts worklogDebts = worklogDebtsService.getAllForPeriod(dateFrom, dateTo);
         return mapper.mapToDto(worklogDebts);
--- src/main/java/com/senla/worklog/reminder/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/senla/worklog/reminder/exception/handler/GlobalExceptionHandler.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -37,6 +39,17 @@ public class GlobalExceptionHandler {
         log.warn("Resolved MissingServletRequestParameterException: {}", e.getMessage());
         String message = "Missing value for parameter '" + e.getParameterName() + "'";
         ApiError apiError = new ApiError(message, BAD_REQUEST);
+        return new ResponseEntity<>(apiError, BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiError> handleException(ConstraintViolationException e) {
+        log.warn("Resolved ConstraintViolationException: {}", e.getMessage());
+        String message = "Validation failed";
+        List<ApiSubError> subErrors = e.getConstraintViolations().stream()
+                .map(violation -> new ApiSubError(violation.getMessage()))
+                .collect(Collectors.toList());
+        ApiError apiError = new ApiError(message, BAD_REQUEST, subErrors);
         return new ResponseEntity<>(apiError, BAD_REQUEST);
     }
 
