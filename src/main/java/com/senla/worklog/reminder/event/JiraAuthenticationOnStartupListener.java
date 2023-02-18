bdd39e51671bc0f804eebeffd322b9e4af948cef
leonid <kotovpetia@gmail.com>
2023-02-18 22:44:05 +0400

    #18 Add JiraAuthenticationOnStartupListener

---
+++ src/main/java/com/senla/worklog/reminder/event/JiraAuthenticationOnStartupListener.java
@@ -0,0 +1,22 @@
+package com.senla.worklog.reminder.event;
+
+import com.senla.worklog.reminder.service.JiraAuthenticationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JiraAuthenticationOnStartupListener implements ApplicationListener<ContextRefreshedEvent> {
+    private final JiraAuthenticationService authenticationService;
+
+    @Override
+    public void onApplicationEvent(@NonNull ContextRefreshedEvent event) {
+        log.info("Login at the application startup");
+        authenticationService.login();
+    }
+}
