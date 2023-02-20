32a44553d30b9843dc8bdf2006733d5e0e575d46
leonid <kotovpetia@gmail.com>
2023-02-20 04:24:19 +0400

    Add JIRA_DEBUG_PASSWORD_ENABLED property

--- src/main/java/com/senla/worklog/reminder/config/JiraProperties.java
+++ src/main/java/com/senla/worklog/reminder/config/JiraProperties.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Component
@@ -16,15 +18,26 @@ import org.springframework.stereotype.Component;
 public class JiraProperties {
     private String worklogsUrlTemplate;
     private String loginUrl;
+    private DebugOptions debug;
     private AuthProperties basicAuth;
     private AuthProperties formAuth;
 
     @Getter
     @Setter
+    @Accessors(chain = true)
     @NoArgsConstructor
     @AllArgsConstructor
     public static class AuthProperties {
         private String username;
         private String password;
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DebugOptions {
+        private boolean passwordEnabled;
     }
 }
