27f7c7b5aaf3c31f7671757603e1e446815b6f7f
leonid <kotovpetia@gmail.com>
2023-02-20 04:39:01 +0400

    Add JiraAuthenticationOnStartupListener passwords hiding

--- src/main/java/com/senla/worklog/reminder/event/JiraAuthenticationOnStartupListener.java
+++ src/main/java/com/senla/worklog/reminder/event/JiraAuthenticationOnStartupListener.java
@@ -27,8 +27,22 @@ public class JiraAuthenticationOnStartupListener implements ApplicationListener<
         log.debug("Using Jira login properties:\n\t" +
                 "jira.login-url                  " + jiraProperties.getLoginUrl() + "\n\t" +
                 "jira.basic-auth.username        " + jiraProperties.getBasicAuth().getUsername() + "\n\t" +
-                "jira.basic-auth.password        " + jiraProperties.getBasicAuth().getPassword() + "\n\t" +
+                "jira.basic-auth.password        " + hideBasicPasswordIfRequired() + "\n\t" +
                 "jira.form-auth.username         " + jiraProperties.getFormAuth().getUsername() + "\n\t" +
-                "jira.form-auth.password         " + jiraProperties.getFormAuth().getPassword() + "\n");
+                "jira.form-auth.password         " + hideFormPasswordIfRequired() + "\n");
+    }
+
+    private String hideBasicPasswordIfRequired() {
+        if (jiraProperties.getDebug().isPasswordEnabled()) {
+            return jiraProperties.getBasicAuth().getPassword();
+        }
+        return "[hidden]";
+    }
+
+    private String hideFormPasswordIfRequired() {
+        if (jiraProperties.getDebug().isPasswordEnabled()) {
+            return jiraProperties.getFormAuth().getPassword();
+        }
+        return "[hidden]";
     }
 }
