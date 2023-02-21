105700fedab4098b22bf68716762f41cc1c98fdb
leonid <kotovpetia@gmail.com>
2023-02-21 23:29:24 +0400

    #21 Add JiraWorklogClientV4 with impl

---
+++ src/main/java/com/senla/worklog/reminder/api/v4/client/JiraWorklogClientV4.java
@@ -0,0 +1,26 @@
+package com.senla.worklog.reminder.api.v4.client;
+
+import com.senla.worklog.reminder.api.v4.model.WorklogV4;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.LocalDate.now;
+
+public interface JiraWorklogClientV4 {
+    String path = "/rest/tempo-timesheets/4/worklogs/search";
+
+    default List<WorklogV4> getAllForCurrentWeek() {
+        return getAllForPeriod(now().with(MONDAY), now().with(FRIDAY));
+    }
+
+    default List<WorklogV4> getAllForPreviousWeek() {
+        LocalDate previousMonday = now().with(MONDAY).minusWeeks(1);
+        LocalDate previousFriday = now().with(FRIDAY).minusWeeks(1);
+        return getAllForPeriod(previousMonday, previousFriday);
+    }
+
+    List<WorklogV4> getAllForPeriod(LocalDate dateFrom, LocalDate dateTo);
+}
