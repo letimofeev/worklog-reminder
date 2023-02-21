9056501f7b3440f89abda819511422b9ae9e2285
leonid <kotovpetia@gmail.com>
2023-02-22 01:46:25 +0400

    RefreshableSessionAspect refactoring

--- src/main/java/com/senla/worklog/reminder/aspect/RefreshableSessionAspect.java
+++ src/main/java/com/senla/worklog/reminder/aspect/RefreshableSessionAspect.java
@@ -10,7 +10,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 
 @Slf4j
 @Aspect
@@ -21,27 +21,36 @@ public class RefreshableSessionAspect {
 
     @Around("@annotation(com.senla.worklog.reminder.annotation.RefreshableSession)")
     public Object aroundRefreshableAnnotatedMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        var methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         int maxRetries = methodSignature.getMethod().getAnnotation(RefreshableSession.class).maxRetries();
         int currentRetries = 0;
         while (currentRetries <= maxRetries) {
             try {
                 return proceedingJoinPoint.proceed();
             } catch (Exception e) {
-                if (e instanceof HttpClientErrorException.Unauthorized || e.getCause() instanceof HttpClientErrorException.Unauthorized) {
-                    if (currentRetries < maxRetries) {
-                        log.warn("Received 401 Unauthorized. Perhaps session is expired, logging in and trying again. Retry number: {}/{}",
-                                currentRetries, maxRetries);
-                        authenticationService.login();
-                    }
-                    currentRetries++;
-                } else {
-                    throw e;
-                }
+                currentRetries = handleException(maxRetries, currentRetries, e);
             }
         }
         log.error("Received 401 Unauthorized after retries with login. Total attempts number: {}", currentRetries);
         throw new JiraAuthenticationException("Jira Api Client 401 Unauthorized after retry with login. " +
                 "Make sure that request was sent with correct headers");
+    }
+
+    private int handleException(int maxRetries, int currentRetries, Exception e) throws Exception {
+        if (isExceptionOrCauseUnauthorized(e)) {
+            if (currentRetries < maxRetries) {
+                log.warn("Received 401 Unauthorized. Perhaps session is expired, logging in and trying again. " +
+                                "Retry number: {}/{}", currentRetries, maxRetries);
+                authenticationService.login();
+            }
+            currentRetries++;
+        } else {
+            throw e;
+        }
+        return currentRetries;
+    }
+
+    private boolean isExceptionOrCauseUnauthorized(Exception e) {
+        return e instanceof Unauthorized || e.getCause() instanceof Unauthorized;
     }
 }
