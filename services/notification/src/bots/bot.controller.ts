a072f233e8f329dfd639d25815af1804fb104a74
leonid <kotovpetia@gmail.com>
2023-03-04 22:46:37 +0400

    #36 Add basic notify all method

--- services/notification/src/bots/bot.controller.ts
+++ services/notification/src/bots/bot.controller.ts
@@ -1,14 +1,37 @@
-import {Controller, Post, Req, Res} from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    Post,
+    Req,
+    Res
+} from "@nestjs/common";
 import {ActivityHandler, CloudAdapter, TurnContext} from "botbuilder";
+import {UserService} from "../users/user.service";
+import * as process from "process";
+import {ConversationReference} from "botframework-schema";
 
-@Controller('/api/bot/messages')
+@Controller('/api/bot')
 export class BotController {
-    constructor(private adapter: CloudAdapter, private activityHandler: ActivityHandler) {
+    constructor(private adapter: CloudAdapter,
+                private activityHandler: ActivityHandler,
+                private userService: UserService) {
     }
 
-    @Post()
-    async sendMessage(@Req() request, @Res() response) {
+    @Post('/messages')
+    async sendActivity(@Req() request, @Res() response) {
         await this.adapter.process(request, response, (context: TurnContext) =>
             this.activityHandler.run(context));
+    }
+
+    @Get('/notify')
+    async notifyAll() {
+        const users = await this.userService.getAll();
+        users.forEach(user => {
+            const conversationReference = user.conversationReference as Partial<ConversationReference>;
+            this.adapter.continueConversationAsync(process.env.MicrosoftAppId, conversationReference,
+                async (context) => {
+                await context.sendActivity('notification');
+            })
+        })
     }
 }
