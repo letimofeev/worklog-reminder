a435b97f0d24969d32c0e08fb0c68d2ddcb26fe1
leonid <kotovpetia@gmail.com>
2023-03-15 21:22:20 +0400

    #48 Add configs

---
+++ services/employee/src/main/java/com/senla/worklog/reminder/employee/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.senla.worklog.reminder.employee.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        var modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        return modelMapper;
+    }
+}
