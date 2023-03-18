9e6f3db8acf8caae6c67c85e57e3dab503e6a8aa
leonid <kotovpetia@gmail.com>
2023-03-18 22:17:53 +0400

    #48 Init worklog-debt-notification

---
+++ services/worklog-debt-notification/src/main/java/com/senla/worklog/reminder/worklogdebtnotification/WorklogDebtNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.senla.worklog.reminder.worklogdebtnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorklogDebtNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorklogDebtNotificationApplication.class, args);
+	}
+
+}
