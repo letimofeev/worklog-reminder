eca89f89ca8b88a4e201cc0e18ee647085be952e
leonid <kotovpetia@gmail.com>
2023-03-24 18:58:26 +0400

    #65 Add javadocs

--- services/employee/src/main/java/com/senla/worklog/reminder/employee/adapter/in/rest/exception/handler/HttpRequestMethodNotSupportedExceptionHandler.java
+++ services/employee/src/main/java/com/senla/worklog/reminder/employee/adapter/in/rest/exception/handler/HttpRequestMethodNotSupportedExceptionHandler.java
@@ -9,9 +9,26 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import static com.senla.worklog.reminder.employee.adapter.in.rest.dto.ApiError.methodNotAllowed;
 import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
 
+/**
+ * An exception handler for handling {@link HttpRequestMethodNotSupportedException}, which is thrown when a
+ * request handler does not support a specific request method. This handler returns a HTTP 405 Method Not Allowed
+ * response with a user-friendly error message.
+ */
 @Slf4j
 @Component
 public class HttpRequestMethodNotSupportedExceptionHandler extends AbstractRestAdapterExceptionHandler {
+
+    /**
+     * Handles the {@link HttpRequestMethodNotSupportedException} and returns a HTTP 405 Method Not Allowed response
+     * with a user-friendly error message
+     * <p>
+     * If the passed exception is not an instance of HttpMessageNotReadableException, it delegates
+     * the handling to the parent class method {@link #handleUnsupportedExceptionType(Exception)} handleUnsupportedExceptionType()}
+     *
+     * @param e the {@link HttpRequestMethodNotSupportedException} to handle
+     * @return a {@link ResponseEntity} containing an {@link ApiError} with a HTTP 405 status code and
+     * a user-friendly error message
+     */
     @Override
     public ResponseEntity<ApiError> handleException(Exception e) {
         if (e instanceof HttpRequestMethodNotSupportedException) {
@@ -23,6 +40,11 @@ public class HttpRequestMethodNotSupportedExceptionHandler extends AbstractRestA
         return handleUnsupportedExceptionType(e);
     }
 
+    /**
+     * Returns the type of exception that this handler can handle, which is {@link HttpRequestMethodNotSupportedException}.
+     *
+     * @return the {@link HttpRequestMethodNotSupportedException} class
+     */
     @Override
     public Class<? extends Throwable> getExceptionType() {
         return HttpRequestMethodNotSupportedException.class;
