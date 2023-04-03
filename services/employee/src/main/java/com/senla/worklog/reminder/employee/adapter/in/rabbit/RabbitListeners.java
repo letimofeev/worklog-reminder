cadb8cb64c6cb5ca4b9e034f9b6c9bd0c6d213e5
leonid <kotovpetia@gmail.com>
2023-04-03 13:50:50 +0400

    #88 Add dead letter queues for rejected messages

--- services/employee/src/main/java/com/senla/worklog/reminder/employee/adapter/in/rabbit/RabbitListeners.java
+++ services/employee/src/main/java/com/senla/worklog/reminder/employee/adapter/in/rabbit/RabbitListeners.java
@@ -17,13 +17,13 @@ import static com.senla.worklog.reminder.employee.adapter.in.rabbit.event.Region
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class RabbitMqListener {
+public class RabbitListeners {
     private final RegionServicePort regionServicePort;
     private final RegionEventDataMapper eventDataMapper;
 
     @RabbitListener(queues = "${rabbitmq.region.queue.created}")
     public void handleRegionCreatedEvent(RegionCreatedEvent event) {
-        log.debug("Processing event from queue for {} events: {}", REGION_CREATED, event);
+        log.debug("Processing {} event: {}", REGION_CREATED, event);
 
         var eventData = event.getData();
         var region = eventDataMapper.mapToDomain(eventData);
@@ -32,7 +32,7 @@ public class RabbitMqListener {
 
     @RabbitListener(queues = "${rabbitmq.region.queue.updated}")
     public void handleRegionUpdatedEvent(RegionUpdatedEvent event) {
-        log.debug("Processing event from queue for {} events: {}", REGION_UPDATED, event);
+        log.debug("Processing {} event: {}", REGION_UPDATED, event);
 
         var eventData = event.getData();
         var region = eventDataMapper.mapToDomain(eventData);
@@ -41,7 +41,7 @@ public class RabbitMqListener {
 
     @RabbitListener(queues = "${rabbitmq.region.queue.deleted}")
     public void handleRegionDeletedEvent(RegionDeletedEvent event) {
-        log.debug("Processing event from queue for {} events: {}", REGION_DELETED, event);
+        log.debug("Processing {} event: {}", REGION_DELETED, event);
 
         var eventData = event.getData();
         var regionId = eventData.getId();
